dd5aced432c8d244ace63c7d39819beea3c56827
nishanthspk <spknishanth2004@gmail.com>
2024-09-04 18:50:27 +0530

    Image Insertion

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,9 +7,9 @@ export default class
  extends PureComponent {
   render() {
     return (
-      <div>
+      <div className=' bg-[#81B5FA] pt-8'>
       <nav >
-      <div className='flex justify-between mx-20 p-2 rounded-3xl mt-8 bg-[#F4F7FF]'>
+      <div className='flex justify-between mx-20  rounded-3xl p-2 bg-[#F4F7FF]'>
       <div className='flex gap-4 mx-4'>
       <img className='h-8' src={Logo} alt="" />
       <div className='pt-1'>ArcVapi.</div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,51 @@
 import React, { PureComponent } from 'react'
+import Globe from '../Assets/Group.png';
 
 export default class Home extends PureComponent {
   render() {
     return (
-      <div className='h-screen bg-[#81B5FA]'>
-      <div >
-      <p className='text-5xl'>Delivering Your Promise, <br /> Every Mile, Every Time.</p>
-      <p>We turn logistics into a seamless journey. With a track record of <br /> reliability and excellence, we ensure your goods reach their destination <br /> safely, efficiently, and on time—every single time.</p>
-      <button className='bg-[#FFFFFF]'>Get Your Quote Now</button>
-      </div>
+      <div>
+        <div className='h-screen font DM Sans bg-[#81B5FA] flex justify-between '>
+          <div className='pt-32 pl-20'>
+            <p className='text-5xl font-semibold '>Delivering Your  Promise, <br /> Every Mile, Every Time.</p>
+            <p className='my-4'>We turn logistics into a seamless journey. With a track record of <br /> reliability and excellence, we ensure your goods reach their destination <br /> safely, efficiently, and on time—every single time.</p>
+            <button className='bg-[#FFFFFF] rounded-lg px-8 py-2 border-b-2 border-black'>Get Your Quote No  →</button>
+          </div>
+          <div className='pt-20'>
+            <img className='' src={Globe} alt="" />
+          </div>
+
+        </div>
+        
+        
+        <div class="bg-blue-50 p-6 rounded-lg shadow-lg max-w-4xl mx-auto border-2 border-b-[#81B5FA]">
+          <h2 class="text-2xl font-bold mb-2">Track Your Shipment</h2>
+          <p class="text-gray-600 mb-4">
+            Enter your tracking number below to monitor the status of your shipment. Our advanced tracking system provides you with up-to-the-minute updates, ensuring you stay informed throughout the entire delivery process.
+          </p>
+          <div class="flex items-center space-x-2">
+            <input
+              type="text"
+              placeholder="Enter the track no."
+              class="w-10/12 p-2 border-2 border-[#81B5FA] rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+            <button
+              class="bg-[#81B5FA] text-white px-4 py-2  rounded-md hover:bg-blue-600 "
+            >
+              Track Now
+            </button>
+          </div>
+        </div>
+        
+        <div className='pt-20'>
+        <p className='font-semibold text-center'>Why Choose Abhay Road Carrier?</p>
+        <p className='text-center'>Five years of expertise, exceptional service, and cutting-edge technology <br /> ensure your logistics needs are met with precision and care.</p>
+        </div>
+        
+        <div>
+        <img src="" alt="" />
+        </div>
+
       </div>
     )
   }
