7123bd534aad799ffa17f94322d34a12957b6848
nishanthspk <spknishanth2004@gmail.com>
2024-09-04 16:23:47 +0530

    Nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 import Logo from "../Assets/Arc.png";
-import { Link } from 'react-router-dom';
+
 
 
 export default class 
@@ -9,16 +9,16 @@ export default class
     return (
       <div>
       <nav >
-      <div className='flex justify-between mx-5'>
-      <div className='flex gap-2'>
-      <img src={Logo} alt="" />
-      <div>ArcVapi.</div>
+      <div className='flex justify-between mx-20 p-2 rounded-3xl mt-8 bg-[#F4F7FF]'>
+      <div className='flex gap-4 mx-4'>
+      <img className='h-8' src={Logo} alt="" />
+      <div className='pt-1'>ArcVapi.</div>
       </div>
-      <div className='flex gap-2'>
-      <p>About Us</p>
-      <p>Contact</p>
+      <div className='flex gap-4 mx-4'>
+      <p className='pt-1'>About Us</p>
+      <p className='pt-1'>Contact</p>
       <div className=''>
-      <button className='bg-[#81B5FA]'>Tools</button>
+      <button className='bg-[#81B5FA] rounded-lg px-2 py-1'>Tools</button>
       </div>
       </div>
       </div>
