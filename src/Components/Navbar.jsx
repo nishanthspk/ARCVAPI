affe55cf3d3ec51b590dfe3b3b8bc7e7251de8e1
nishanthspk <spknishanth2004@gmail.com>
2024-09-04 19:26:15 +0530

    Cards

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,18 +7,18 @@ export default class
  extends PureComponent {
   render() {
     return (
-      <div className=' bg-[#81B5FA] pt-8'>
+      <div className='font DM sans bg-[#81B5FA] pt-8'>
       <nav >
       <div className='flex justify-between mx-20  rounded-3xl p-2 bg-[#F4F7FF]'>
       <div className='flex gap-4 mx-4'>
       <img className='h-8' src={Logo} alt="" />
-      <div className='pt-1'>ArcVapi.</div>
+      <div className='pt-1 font-semibold'>ArcVapi.</div>
       </div>
       <div className='flex gap-4 mx-4'>
-      <p className='pt-1'>About Us</p>
-      <p className='pt-1'>Contact</p>
+      <p className='pt-1 mx-4'>About Us</p>
+      <p className='pt-1 mx-4'>Contact</p>
       <div className=''>
-      <button className='bg-[#81B5FA] rounded-lg px-2 py-1'>Tools</button>
+      <button className='bg-[#81B5FA] rounded-2xl px-4 py-1 text-white'>Tools</button>
       </div>
       </div>
       </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
 import React, { PureComponent } from 'react'
 import Globe from '../Assets/Group.png';
+import Arc from '../Assets/Arc.png';
+import Cpu from '../Assets/cpu.png';
+import User from '../Assets/user-octagon.png';
+
+
 
 export default class Home extends PureComponent {
   render() {
@@ -38,12 +43,29 @@ export default class Home extends PureComponent {
         </div>
         
         <div className='pt-20'>
-        <p className='font-semibold text-center'>Why Choose Abhay Road Carrier?</p>
-        <p className='text-center'>Five years of expertise, exceptional service, and cutting-edge technology <br /> ensure your logistics needs are met with precision and care.</p>
+        <p className='font-semibold text-center text-4xl '>Why Choose Abhay Road Carrier?</p>
+        <p className='text-center my-4'>Five years of expertise, exceptional service, and cutting-edge technology <br /> ensure your logistics needs are met with precision and care.</p>
         </div>
         
-        <div>
-        <img src="" alt="" />
+        <div className='flex gap-10 pt-20 mx-28 '>
+        <div className='border-2 border-r-[#81B5FA]'>
+        <img className='my-2' src={Arc} alt="" />
+        <p className='text-xl font-semibold my-2'>Customer-Centric Approach</p>
+        <p className='items-center my-2'>Our focus is on delivering exceptional service and building long-term relationships with our clients.</p>
+        </div>
+        
+        
+        <div className='border-2 border-r-[#81B5FA]'>
+        <img className='my-2' src={Cpu} alt="" />
+        <p className='text-xl font-semibold my-2'>Experience and Expertise</p>
+        <p className='items-center my-2'>With five years in the logistic industry, we have the knowledge and skills to handle any transportation challenge.</p>
+        </div>
+        
+        <div className='border-2 border-r-[#81B5FA]'>
+        <img className='my-2' src={User} alt="" />
+        <p className='text-xl font-semibold my-2'>Advanced Technology</p>
+        <p className='items-center my-2'>Stay connected with real-time tracking and seamless communication throughout your shipment's journey.</p>
+        </div>
         </div>
 
       </div>
