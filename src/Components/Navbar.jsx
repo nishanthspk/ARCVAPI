dda221a0e0f1010bd4812ce0ba1100f5cb43011a
nishanthspk <spknishanth2004@gmail.com>
2024-09-06 23:10:15 +0530

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,18 +7,18 @@ export default class
  extends PureComponent {
   render() {
     return (
-      <div className='font DM sans bg-[#81B5FA] pt-8'>
+      <div className='font DM sans bg-[#81B5FA] pt-4 md:pt-8'>
       <nav >
-      <div className='flex justify-between mx-20  rounded-3xl p-2 bg-[#F4F7FF]'>
-      <div className='flex gap-4 mx-4'>
-      <img className='h-8' src={Logo} alt="" />
-      <div className='pt-1 font-semibold'>ArcVapi.</div>
+      <div className='flex justify-between md:mx-20  rounded-3xl md:p-2 bg-[#F4F7FF]'>
+      <div className='flex md:gap-4 md:mx-4'>
+      <img className='md:h-8' src={Logo} alt="" />
+      <div className='md:pt-1 font-semibold'>ArcVapi.</div>
       </div>
-      <div className='flex gap-4 mx-4'>
-      <p className='pt-1 mx-4  hover:bg-[#81B5FA] rounded-xl px-2 py-1 '>About Us</p>
-      <p className='pt-1 mx-4 hover:bg-[#81B5FA] rounded-xl px-2 py-1 '>Contact</p>
+      <div className='flex md:gap-4 md:mx-4'>
+      <p className='md:pt-1 md:mx-4  hover:bg-[#81B5FA] rounded-xl md:px-2 md:py-1 '>About Us</p>
+      <p className='md:pt-1 md:mx-4 hover:bg-[#81B5FA] rounded-xl md:px-2 md:py-1 '>Contact</p>
       <div className=''>
-      <button className='bg-[#81B5FA] rounded-2xl px-4 py-1 text-white hover:bg-blue-500'>Tools</button>
+      <button className='bg-[#81B5FA] rounded-2xl md:px-4 md:py-1 text-white hover:bg-blue-500'>Tools</button>
       </div>
       </div>
       </div>
