369d4ca6bb648e2866b37558efe96ae30a86d644
nishanthspk <spknishanth2004@gmail.com>
2024-09-06 23:13:56 +0530

    spacing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,12 +9,12 @@ export default class
     return (
       <div className='font DM sans bg-[#81B5FA] pt-4 md:pt-8'>
       <nav >
-      <div className='flex justify-between md:mx-20  rounded-3xl md:p-2 bg-[#F4F7FF]'>
-      <div className='flex md:gap-4 md:mx-4'>
-      <img className='md:h-8' src={Logo} alt="" />
-      <div className='md:pt-1 font-semibold'>ArcVapi.</div>
+      <div className='flex justify-between md:mx-20  rounded-3xl p-2 md:p-2 bg-[#F4F7FF]'>
+      <div className='flex md:gap-4 md:mx-4 gap-2 mx-2'>
+      <img className='md:h-8 h-7 ' src={Logo} alt="" />
+      <div className='md:pt-1 font-semibold '>ArcVapi.</div>
       </div>
-      <div className='flex md:gap-4 md:mx-4'>
+      <div className='flex md:gap-4 md:mx-4 gap-2'>
       <p className='md:pt-1 md:mx-4  hover:bg-[#81B5FA] rounded-xl md:px-2 md:py-1 '>About Us</p>
       <p className='md:pt-1 md:mx-4 hover:bg-[#81B5FA] rounded-xl md:px-2 md:py-1 '>Contact</p>
       <div className=''>
