0f12845974459185cc7cf8c7c310bb78bb8799e8
nishanthspk <spknishanth2004@gmail.com>
2024-09-09 11:56:35 +0530

    Center

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -54,8 +54,8 @@ export default class Home extends PureComponent {
         </div>
       </div>
 
-        <div className='pt-40'>
-          <p className='font-semibold text-center text-4xl '>Why Choose Abhay Road Carrier?</p>
+        <div className='pt-40 md:pt-40'>
+          <p className='font-semibold md:text-center md:text-4xl text-xl text-center'>Why Choose Abhay Road Carrier?</p>
           <p className='text-center md:text-center my-4'>Five years of expertise, exceptional service, and cutting-edge technology  ensure your logistics needs are met with precision and care.</p>
         </div>
 
